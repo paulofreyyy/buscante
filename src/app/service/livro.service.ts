1599801dd200f1855fe9b2096c5e2f7af70cf36a
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-06-03 23:12:28 -0300

    feat: criando service para buscar livros

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LivroService {
+    // Url da API google books
+    private readonly API = 'https://www.googleapis.com/books/v1/volumes'
+    constructor(private http: HttpClient) { }
 
-  constructor() { }
+    // Função de buscar livros de acordo com valor informado no front
+    buscar(valorDigitado: string) {
+        const params = new HttpParams().append('q', valorDigitado)
+        return this.http.get(this.API, { params })
+    }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
-  selector: 'app-lista-livros',
-  templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+    selector: 'app-lista-livros',
+    templateUrl: './lista-livros.component.html',
+    styleUrls: ['./lista-livros.component.css']
 })
 export class ListaLivrosComponent {
 
-  listaLivros: [];
+    listaLivros: [];
+    campoBusca: string = ''
 
-  constructor() { }
+    constructor(private service: LivroService) { }
+
+    // Função para realizar a busca por livros criada na service
+    buscarLivros(){
+        this.service.buscar(this.campoBusca)
+    }
 
 }
 
